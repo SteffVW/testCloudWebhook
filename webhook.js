34e5ceeeb368ed59fce6d24e5447ee5e21495b10
SteffVW <159015001+SteffVW@users.noreply.github.com>
2024-05-08 14:54:21 +0200

    Add files via upload

---
+++ webhook.js
@@ -0,0 +1,31 @@
+import express from 'express';
+
+const app = express();
+const PORT = 80;
+app.use(express.json());
+// dit is geen Github secret
+// deze code runt normaal op een andere machine dan de workflow
+const VALID_SECRET = 'my-secret-pw';
+
+app.post('/webhook', (req, res) => {
+    const auth_header = req.headers.authorization;
+    if (auth_header && auth_header.startsWith("Bearer ")) {
+        const token = auth_header.substring(7);
+        if (token === VALID_SECRET) {
+            console.log('Received valid webhook:');
+            res.status(200).send('Webhook processed successfully!');
+        } else {
+            console.log("Recieved invalid webhook");
+            res.status(403).send("Invalid secret");
+        }
+    }
+    else {
+        console.log("Recieved invalid webhook");
+        res.status(403).send("Missing secret");
+    }
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
